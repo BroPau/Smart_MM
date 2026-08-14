5410a28d3a110ff9bb302ea338611cb436706934
LW0755 <v61983@gmail.com>
2026-08-14 18:56:14 +0800

    Initial Commit

---
+++ Smart Meeting Minutes/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Smart Meeting Minutes
+//
+//  Created by Weilu on 8/14/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
